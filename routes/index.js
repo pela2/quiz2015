75e526f6521d9a0b423e34f8678e4cafdb1c8dc3
pela2.com <damian.matteucci@gmail.com>
2015-07-07 21:49:14 -0300

    primera pagina y favicon 2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Quiz 2015' });
 });
 
 module.exports = router;
